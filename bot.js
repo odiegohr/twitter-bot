519a92bf43af884d518fd269eeb6154d7141d663
Diego <diegohriquetti@gmail.com>
2019-07-25 20:12:19 -0300

    Metodo para twettar implementado

---
+++ bot.js
@@ -0,0 +1,14 @@
+var Twit = require('twit')
+
+var TwBot = new Twit({
+  consumer_key: 'ucZRXkt8j36XCvt9gBWm7Fx6A',
+  consumer_secret: 'L3BbUJ1wCnJOBoHXzMclJuRKvWp6ktEhAxCiaCGIqf4gUlcOlv',
+  access_token: '3410648987-LiDbfEKjwR8a4ATUfzmLIFf2awwGDg3e2TcNcCS',
+  access_token_secret: 'rDiN3E5v7rQnKX99uYtGFQGOBc0d9t0YOCKgQUKAkLoFZ'
+})
+
+
+//Twittar alguma coisa:
+TwBot.post('statuses/update', { status: 'aehhh funciono' } )
+    
+  
